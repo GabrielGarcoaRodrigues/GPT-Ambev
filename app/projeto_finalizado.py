bb6e47006df457c89ec3eac723722ece293c4239
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2024-08-02 15:10:20 -0300

    Update projeto_finalizado.py

--- app/projeto_finalizado.py
+++ app/projeto_finalizado.py
@@ -4,7 +4,7 @@ import openpyxl
 import openai
 import os
 import re
-
+from PyPDF2 import PdfReader
 from utils_openai import retorna_resposta_modelo
 from utils_files import *
 
@@ -22,6 +22,8 @@ def inicializacao():
         st.session_state.api_key = le_chave()
     if 'df_texto' not in st.session_state:
         st.session_state.df_texto = None
+    if 'texto' not in st.session_state:  # Inicializar 'texto' caso não exista
+        st.session_state.texto = None
 
     chave = "sua chave"
     if chave != st.session_state['api_key']:
@@ -53,17 +55,23 @@ def handle_uploaded_file(uploaded_file, limit=5000):
                 return df
             else:
                 st.error("A coluna 'Texto' não foi encontrada no arquivo.")
+
     except Exception as e:
-        st.error(f"Erro: {e}")
+        st.error("A coluna 'Texto' não foi encontrada no arquivo.")
     return None
+
+def handle_uploaded_file_pdf(uploaded_file):
+    text=""
+    pdf_reader= PdfReader(uploaded_file)
+    for page in pdf_reader.pages:
+        text+= page.extract_text()
+    return  text
 
 
 def display_results(results):
     if results:
         results_str = ''.join(results)
         st.write(results_str)
-
-col1, col2 = st.columns([3,1])
 
 
 def pagina_principal():
@@ -74,11 +82,11 @@ def pagina_principal():
     if st.session_state.get('show_info', False):
         with st.expander("Como utilizar o chat"):
             st.write("""
-               Esse chatbot é um GPT Geral, feito para conversar livremente sobre qualquer assunto.
+               Esse chatbot é um GPT Geral, feito para conversar livremente sobre qualquer assunto, podendo enviar arquivos .xlsx ou PDF para análise.
             """)
         with st.expander("Tipo de arquivo"):
             st.write("""
-                Até o momento o chatbot aceita apenas arquivos Excel (.xlsx) com uma coluna chamada 'Texto' contendo os comentários.
+                O chatbot aceita apenas arquivos Excel (.xlsx) com uma coluna chamada 'Texto' contendo os comentários OU arquivo PDF.
             """)
         with st.expander("Comentários removidos"):
             st.write("""
@@ -86,9 +94,16 @@ def pagina_principal():
             """)
 
     # Carregar o arquivo e processar o conteúdo
-    uploaded_file = st.file_uploader("Faça o upload do arquivo Excel com os comentários sobre a crise", type='xlsx', help="O arquivo deve conter uma coluna chamada 'Texto'.", accept_multiple_files=False)
-    if uploaded_file:
-        st.session_state.df_texto = handle_uploaded_file(uploaded_file)
+    uploaded_file = st.file_uploader("Faça o upload do arquivo Excel ou PDF com os comentários sobre a crise", help="O arquivo deve conter uma coluna chamada 'Texto'.", accept_multiple_files=False)
+    if uploaded_file:  
+        if uploaded_file.name.endswith('.xlsx'):
+            st.session_state.df_texto = handle_uploaded_file(uploaded_file)
+
+        elif uploaded_file.name.endswith('.pdf'):
+            st.session_state.texto = handle_uploaded_file_pdf(uploaded_file) # Trocar essa funçao para ler pdf
+        
+        else:
+            st.warning("Por favor, faça o upload de um arquivo Excel (.xlsx) ou PDF.")
     
     # Mostrar histórico da conversa
     for mensagem in st.session_state.mensagens:
@@ -107,10 +122,18 @@ def pagina_principal():
             st.session_state.mensagens.append(nova_mensagem_usuario)
             st.chat_message('user').markdown(prompt)
             
+            # Inicializar 'comentarios_texto'
+            comentarios_texto = ""
+
             # Adicionar conteúdo do arquivo ao prompt se existir
-            if st.session_state.df_texto is not None:
+            if st.session_state.df_texto is not None and not st.session_state.df_texto['Texto'].empty:
                 comentarios_texto = '\n'.join(st.session_state.df_texto['Texto'].tolist())
                 prompt_completo = f"{prompt}\n\nComentários do arquivo:\n{comentarios_texto}"
+        
+           
+            elif st.session_state.texto is not None:
+                prompt_completo = f"{prompt}\n\nArquivo PDF:\n{st.session_state.texto}"
+            
             else:
                 prompt_completo = prompt
 
